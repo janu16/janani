fe3395c2cdaf0f94f193e226a0bcaa4c5c9d3fe8
janu16 <januthangavelu7@gmail.com>
2016-07-06 12:41:54 +0530

    Create greater.c

---
+++ greater.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("\n enter the values:");
+scanf("%d%d%d",&a,&b,&c);
+if(a>b&&a>c)
+{
+printf("\n a is greater:");
+}
+elseif(b>c){
+printf("\n b is greater");
+}
+else{
+ptintf("\n c is greater");
+}
+getch();
+}
